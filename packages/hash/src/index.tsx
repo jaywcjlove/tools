220bc5d57eca411f474ffbbf73f8407199165ad2
jaywcjlove <398188662@qq.com>
2022-05-17 13:07:39 +0800

    feat: update hash featrue.

--- packages/hash/src/index.tsx
+++ packages/hash/src/index.tsx
@@ -1,22 +1,26 @@
 import { useState } from 'react';
 import { Wrapper, Textarea, StyledLayout, CopyButton, ResultCode } from '@wcj/tools-react-components';
 import md5 from 'crypto-js/md5';
+import sha224 from 'crypto-js/sha224';
 import sha256 from 'crypto-js/sha256';
 import sha512 from 'crypto-js/sha512';
+import sha384 from 'crypto-js/sha384';
 import sha1 from 'crypto-js/sha1';
+import ripemd160 from 'crypto-js/ripemd160';
 
 export default function Hash() {
   const [value, setValue] = useState('');
   const handleChange = (evn: React.ChangeEvent<HTMLTextAreaElement>) => {
     const str = (evn.target as HTMLTextAreaElement).value || '';
-    if (str) {
-      setValue(str);
-    }
+    setValue(str);
   };
   const md5_Str = value ? md5(value).toString() : '';
   const sha1_Str = value ? sha1(value).toString() : '';
+  const sha224_Str = value ? sha224(value).toString() : '';
   const sha256_Str = value ? sha256(value).toString() : '';
   const sha512_Str = value ? sha512(value).toString() : '';
+  const sha384_Str = value ? sha384(value).toString() : '';
+  const ripemd160_Str = value ? ripemd160(value).toString() : '';
   return (
     <Wrapper>
       <StyledLayout title="Input" extra={value && <CopyButton value={value} />}>
@@ -29,11 +33,20 @@ export default function Hash() {
         <ResultCode title="SHA1" extra={sha1_Str && <CopyButton value={sha1_Str} />}>
           {sha1_Str}
         </ResultCode>
+        <ResultCode title="SHA224" extra={sha224_Str && <CopyButton value={sha224_Str} />}>
+          {sha224_Str}
+        </ResultCode>
+        <ResultCode title="SHA384" extra={sha384_Str && <CopyButton value={sha384_Str} />}>
+          {sha384_Str}
+        </ResultCode>
         <ResultCode title="SHA256" extra={sha256_Str && <CopyButton value={sha256_Str} />}>
           {sha256_Str}
         </ResultCode>
         <ResultCode title="SHA512" extra={sha512_Str && <CopyButton value={sha512_Str} />}>
           {sha512_Str}
+        </ResultCode>
+        <ResultCode title="RIPEMD160" extra={ripemd160_Str && <CopyButton value={ripemd160_Str} />}>
+          {ripemd160_Str}
         </ResultCode>
       </StyledLayout>
     </Wrapper>
