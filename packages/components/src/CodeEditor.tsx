176957823095f8b321d81b3e5296d8941cea021e
jaywcjlove <398188662@qq.com>
2022-06-11 11:28:37 +0800

    chore(deps): update dependency @uiw/react-codemirror to v^4.8.1

--- packages/components/src/CodeEditor.tsx
+++ packages/components/src/CodeEditor.tsx
@@ -1,19 +1,19 @@
 import { forwardRef, useEffect, useState } from 'react';
-import CodeMirror, { ReactCodeMirrorRef, ReactCodeMirrorProps, EditorView } from '@uiw/react-codemirror';
-
-const borderRadius = EditorView.theme({
-  '.cm-scroller, &': {
-    borderRadius: '6px',
-  },
-  '&': {
-    border: '1px solid var(--color-border-default)',
-  },
-});
+import CodeMirror, { ReactCodeMirrorRef, ReactCodeMirrorProps } from '@uiw/react-codemirror';
+import { EditorView } from '@codemirror/view';
 
 const defalutTheme = document.documentElement.getAttribute('data-color-mode') as Theme;
 type Theme = ReactCodeMirrorProps['theme'];
 
 export const CodeEditor = forwardRef<ReactCodeMirrorRef, ReactCodeMirrorProps>((props, ref) => {
+  const borderRadius = EditorView.theme({
+    '.cm-scroller, &': {
+      borderRadius: '6px',
+    },
+    '&': {
+      border: '1px solid var(--color-border-default)',
+    },
+  });
   const { extensions = [], theme: pTheme } = props;
   const [theme, setTheme] = useState<Theme>(pTheme || defalutTheme);
   useEffect(() => {
